a495573564feec8700ea5c3a99524bff4777e6c5
VinayGoudJ <vjagiri@rivier.edu>
2025-02-27 20:22:29 -0500

    #New changes in home page

--- src/webparts/aiSmartHomeMs/components/AiSmartHomeMs.tsx
+++ src/webparts/aiSmartHomeMs/components/AiSmartHomeMs.tsx
@@ -22,17 +22,13 @@ export default class AiSmartHomeMs extends React.Component<IAiSmartHomeMsProps> 
           <div>Web part property value: <strong>{escape(description)}</strong></div>
         </div>
         <div>
-          <h3>Welcome to SharePoint Framework!</h3>
+          <h2>Welcome to SharePoint Framework CRUD Operations</h2>
           <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+            It is extensibility model for Microsoft Viva, Microsoft Teams and SharePoint.
           </p>
           <h4>Learn more about SPFx development:</h4>
           <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
+           
             <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
             <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
           </ul>
