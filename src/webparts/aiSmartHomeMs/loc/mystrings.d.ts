092c4d85f77005dde167247ff4503f38aefeb6fe
VinayGoudJ <vjagiri@rivier.edu>
2025-02-27 19:15:44 -0500

    Initial commit - Pushing Dev branch

---
+++ src/webparts/aiSmartHomeMs/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IAiSmartHomeMsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'AiSmartHomeMsWebPartStrings' {
+  const strings: IAiSmartHomeMsWebPartStrings;
+  export = strings;
+}
